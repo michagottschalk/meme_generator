6dacde7680450d656100c9290428766badeabd38
Michael Gottschalk <kontakt@micha-gottschalk.de>
2023-03-27 15:32:11 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [meme, setMeme] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    async function fetchMeme() {
+      try {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        const json = await response.json();
+        setMeme(json.data.memes);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchMeme();
+  }, []);
+
+  function handlePrevious() {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  }
+
+  function handleNext() {
+    if (currentIndex < meme.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  }
+
+  return (
+    <div className="meme_body">
+      <h3>MAKE YOUR OWN MEME!</h3>
+      {meme.length > 0 ? (
+        <div className="meme">
+          <div className="meme_img">
+            <img src={meme[currentIndex].url} alt="" />
+          </div>
+          <div className="buttons">
+            <button onClick={handlePrevious}>Zurück</button>
+            <button onClick={handleNext}>Vor</button>
+          </div>
+        </div>
+      ) : (
+        <p>Loading Meme ...</p>
+      )}
+      <div className="type">
+        <form>
+          <input type="text" className="field_one" />
+          <input type="text" className="field_two" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
